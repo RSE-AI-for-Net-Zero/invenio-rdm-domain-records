bc4b315a1f8c315106b0808b9a5f3a0ac7c8a18d
Lee Benson <lee.benson@imperial.ac.uk>
2025-02-05 11:26:28 +0000

    Index mappings

--- src/invenio_rdm_domain_records/records/api.py
+++ src/invenio_rdm_domain_records/records/api.py
@@ -1,14 +1,19 @@
 from invenio_rdm_records.records.api import RDMDraft, RDMRecord
 from invenio_records.systemfields import ConstantField
+from invenio_records_resources.records.systemfields import IndexField
 
 class DomainRDMDraft(RDMDraft):
     # Remember to update INDEXER_DEFAULT_INDEX in Invenio-App-RDM if you
     # update the JSONSchema and mappings to a new version.
     schema = ConstantField("$schema", "local://records/domain-record-v6.0.0.json")
-
+    index = IndexField("rdmdomainrecords-drafts-draft-v6.0.0", search_alias="rdmdomainrecords")
 
 class DomainRDMRecord(RDMRecord):
     # Remember to update INDEXER_DEFAULT_INDEX in Invenio-App-RDM if you
     # update the JSONSchema and mappings to a new version.
     schema = ConstantField("$schema", "local://records/domain-record-v6.0.0.json")
+    index = IndexField(
+        "rdmdomainrecords-records-record-v6.0.0", search_alias="rdmdomainrecords-records"
+    )
+        
 
